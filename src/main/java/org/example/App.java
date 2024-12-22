52e085a62804a89456e6e6bea17ac772eb3c4b1c
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-12-22 22:19:38 +0200

    chore: setup app to run parser

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,14 @@
 package org.example;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         // Your Calculator class's main method already has the testing logic
         System.out.println("Starting the application");
-        Calculator.main(args);
-
+//        Calculator.main(args);
+        // Initialize JavaParser with the file content
+        JavaParser.main(args);
+//        parser.CompilationUnit(); // Start parsing process
+        System.out.println("File parsed succesffuly");
         // Now you can type expressions like:
         // 42 + 10 - 5
         // (Press Enter, then Ctrl+D or Ctrl+Z to send EOF)
